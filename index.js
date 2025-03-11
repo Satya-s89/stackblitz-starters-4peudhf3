f2174b077cd832ace6c54afa9d8839696f1eae5d
Satya-s89 <kaki.harisatya.s89@kalvium.community>
2025-03-11 11:43:01 +0530

    7

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const { resolve } = require('path');
+require('dotenv').config(); 
+
+const isAdmin = process.env.IS_ADMIN === 'true'; 
 
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
 
+
 app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+  if (isAdmin) {
+    res.json({
+      message: "Welcome, Admin!",
+      data: ["Admin Data 1", "Admin Data 2"]
+    });
+  } else {
+    res.json({
+      message: "Welcome, User!",
+      data: ["User Data 1", "User Data 2"]
+    });
+  }
 });
 
 app.listen(port, () => {
